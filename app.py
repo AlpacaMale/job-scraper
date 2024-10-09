ad4b7ad6df082ceb0af35494c3c3f0402a833aaf
AlpacaMale <alpaca9100@gmail.com>
2024-10-09 15:58:15 +0900

    function add export and download result

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from remoteok import scrape_remoteok
 from weworkremotely import scrape_wwr
+from file import save_to_file
 
 app = Flask("JobScrapper")
+
+db = {}
 
 
 @app.route("/")
@@ -13,10 +16,27 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    wwr = scrape_wwr(keyword)
-    remote = scrape_remoteok(keyword)
-    jobs = wwr + remote
+    if keyword == None or keyword == "":
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        wwr = scrape_wwr(keyword)
+        remote = scrape_remoteok(keyword)
+        jobs = wwr + remote
+        db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None or keyword == "":
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 app.run(debug=True)
--- weworkremotely.py
+++ weworkremotely.py
@@ -7,11 +7,11 @@ all_jobs = []
 def scrape_wwr(keyword):
     url = f"https://weworkremotely.com/remote-jobs/search?term={keyword}"
     response = requests.get(url)
-    soup = BeautifulSoup(
-        response.content,
-        "html.parser",
-    )
-    jobs = soup.find("section", class_="jobs").find_all("li")[1:]
+    soup = BeautifulSoup(response.content, "html.parser")
+    jobs = soup.find("section", class_="jobs")
+    if jobs is None:
+        return []
+    jobs = jobs.find_all("li")
     for job in jobs:
         title_data = job.find("span", class_="title")
         if title_data == None:
