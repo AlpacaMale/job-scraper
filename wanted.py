1d20087f6422b160fd7ca74ebd073b3f9ef05520
AlpacaMale <alpaca9100@gmail.com>
2025-01-31 16:41:21 +0900

    refac: scrape wanted

--- wanted.py
+++ wanted.py
@@ -1,12 +1,14 @@
 from playwright.sync_api import sync_playwright
 import time
 from bs4 import BeautifulSoup
+from config import user_agent
 
 
 def scrape_wanted(keyword):
     p = sync_playwright().start()
     browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
+    context = browser.new_context(extra_http_headers={"User-Agent": user_agent})
+    page = context.new_page()
     page.goto(f"https://www.wanted.co.kr/search?query={keyword}&tab=position")
     time.sleep(2)
     for _ in range(3):
@@ -15,16 +17,17 @@ def scrape_wanted(keyword):
     content = page.content()
     p.stop()
     soup = BeautifulSoup(content, "html.parser")
-    jobs = soup.find_all("div", class_="JobCard_container__REty8")
-    all_jobs = []
+    jobs = soup.select(".JobCard_container__REty8")
+    job_info = []
     for job in jobs:
-        link = f"https://www.wanted.co.kr/{job.find('a')['href']}"
-        title = job.find("strong", class_="JobCard_title__HBpZf").text
-        company = job.find("span", class_="JobCard_companyName__N1YrF").text
-        job = {
-            "title": title,
-            "company": company,
-            "link": link,
-        }
-        all_jobs.append(job)
-    return all_jobs
+        title = job.select_one("strong").text
+        company = job.select_one(".JobCard_companyName__N1YrF").text
+        link = job.select_one("a")["href"]
+        job_info.append(
+            {
+                "title": title,
+                "company": company,
+                "link": f"https://www.wanted.co.kr{link}",
+            }
+        )
+    return job_info
